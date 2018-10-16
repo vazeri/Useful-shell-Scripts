d7b4c98dfc7579b08aa62369cab37e0b46c15d12
Goat <erick@ahorratec.org>
2018-10-16 13:42:42 -0500

    Carga de Template en ingles para futuros Shells

---
+++ ShellTemplate.sh
@@ -0,0 +1,41 @@
+#!/bin/Bash
+#
+# SCRIPT:	NAME_of_SCRIPT
+# AUTHOR: 	Erick Vazquez
+# DATE: 	16 Oct 2018
+# REV: 		1.0A	
+# PLATFORM: 	Linux RedHat 
+#
+# PURPOSE: 	Allow the user to upload the information given 
+# 		on a spool to an external sFTP server
+#
+# REV LIST:
+# DATE: 	DATE_of_REVISION
+# BY: 		AUTHOR_of_MODIFICATION
+# MODIFICATION: Describe what was modified, new features, etc--
+#
+#
+# set -n	# Uncomment to check script syntax, without execution.
+# 		# NOTE: Do not forget to put the comment back in or
+# 		# the shell script will not execute!
+# set -x 	# Uncomment to debug this shell script
+#
+##########################################################
+# DEFINE FILES AND VARIABLES HERE
+##########################################################
+
+
+
+##########################################################
+# DEFINE FUNCTIONS HERE
+##########################################################
+
+
+
+##########################################################
+# BEGINNING OF MAIN
+##########################################################
+
+
+
+# End of script
